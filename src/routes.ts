21edde657a4d9dcec546b477e122e5636bffabed
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-09-07 09:24:58 -0300

    Tabela e rota de empresas completo

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import  express  from 'express'
 import candidatesController from './controllers/candidatescontroller'
-import { companiesController } from './controllers/companies-controller'
+import {companiesController} from './controllers/companies-controller'
 
 const router = express.Router()
 
@@ -12,6 +12,10 @@ router.put('/candidates/:id', candidatesController.update)
 router.delete('/candidates/:id', candidatesController.delete)
 
 router.get('/companies', companiesController.index)
+router.post('/companies', companiesController.save)
+router.get('/companies/:id', companiesController.show)
+router.put('/companies/:id', companiesController.update)
+router.delete('/companies/:id', companiesController.delete)
 
 
 export {router}
